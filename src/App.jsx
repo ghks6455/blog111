64790c7c17e3a1d3cacf186ba8b74844048d3f73
ghks6455 <ghks6455@gmail.com>
2024-09-24 17:01:13 +0900

    noooo

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import dummyData from "./js/DUMMY_DATA";
 import Header from "./components/Header";
 import Card from "./components/Card";
 import InputForm from "./components/InputForm";
+import Weather from "./components/Weather";
 function App() {
   const [showCard, setShowCard] = useState(true);
   const [data, setData] = useState("trending");
@@ -19,32 +20,20 @@ function App() {
       localStorage.setItem("blog", JSON.stringify(dummyData));
       // 로컬스토리지에서 데이터 가져오기
       data = JSON.parse(localStorage.getItem("blog"));
+      setVelogContents(dummyData);
     } else {
+      console.log(data);
       setVelogContents(data);
     }
   }, []);
 
   return (
     <>
-      <Header
-        setData={setData}
-        data={data}
-        setShowCard={setShowCard}
-        showCard={showCard}
-      />
+      <Header setData={setData} data={data} setShowCard={setShowCard} showCard={showCard}>
+        <Weather />
+      </Header>
 
-      <main>
-        {showCard ? (
-          velogContents[data].map((post) => (
-            <Card key={post.id} postData={post} data={data} />
-          ))
-        ) : (
-          <InputForm
-            setVelogContents={setVelogContents}
-            setShowCard={setShowCard}
-          />
-        )}
-      </main>
+      <main>{showCard ? velogContents[data].map((post) => <Card key={post.id} data={data} postData={post} setVelogContents={setVelogContents} velogContents={velogContents} />) : <InputForm setVelogContents={setVelogContents} setShowCard={setShowCard} data={data} setData={setData} />}</main>
     </>
   );
 }
